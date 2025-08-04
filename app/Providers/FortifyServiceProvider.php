ff6e9e8f55668fed76e8381c6f3bea4af1baf812
Valentino banyu ampar paraya <144188174+nyukkiw@users.noreply.github.com>
2025-08-04 10:49:32 +0700

    finished resetPass and mail send

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -53,6 +53,17 @@ class FortifyServiceProvider extends ServiceProvider
             return view('auth.login');
         });
 
+        Fortify::requestPasswordResetLinkView(function(){
+            return view('auth.forgot-password');
+        });
+
+        Fortify::resetPasswordView(function ($request){
+            return view('auth.reset-password',['request' => $request]);
+        });
+
+
+
+        
 
     }
 }
